cc6854ddaf2133142cc0d8006a2960bd4d1811bd
Abhijeet Tarale <abhijeettarale@gmail.com>
2016-08-25 10:13:16 +0530

    added search functionality and changed default dext

--- public/client/controller/donationController.js
+++ public/client/controller/donationController.js
@@ -62,7 +62,7 @@ easyDonations.controller('donationController',['$scope', '$http', '$sessionStora
                 */
                     if($scope.posts[i].posted_by ==$scope.donors[j]._id)
                     {
-                        $scope.postDetails.push({"name":$scope.donors[j].name,"items":$scope.posts[i].items,"quantity":$scope.posts[i].quantity,"postedBy":$scope.posts[i].posted_by,"postId":$scope.posts[i]._id});
+                        $scope.postDetails.push({"name":$scope.donors[j].name,"items":$scope.posts[i].items,"quantity":$scope.posts[i].quantity,"postedBy":$scope.posts[i].posted_by,"postId":$scope.posts[i]._id,"loaction":$scope.donors[j].address.city});
                         console.log("postDetails");
                         
                     }
